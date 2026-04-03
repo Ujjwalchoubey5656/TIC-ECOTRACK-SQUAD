44c9cf4aa00e5a643b6d2fa457cd2a896c9ede2f
prasadanshika92 <prasadanshika92@gmail.com>
2026-04-03 23:04:29 +0530

    Add emission feedback messages based on total CO2

--- app.py
+++ app.py
@@ -46,3 +46,14 @@ electricity_co2 = electricity * ELECTRICITY_CO2
 food_co2 = 2 if food == "Veg" else 5
 
 total = transport_co2 + electricity_co2 + food_co2
+#looping
+if total > 10:
+    st.warning("Reduce emissions! Use public transport & save electricity.")
+else:
+    st.success("Great job! You're eco-friendly!")
+
+if food == "Non-Veg":
+    st.write("Try plant-based meals 2–3 times a week")
+
+if electricity > 5:
+    st.write("Turn off unused appliances")

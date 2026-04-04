e919770d83cb904849d3b3b3efbeba8b4f985650
Saurabh Kumar <saurabh7070006797@gmail.com>
2026-04-04 16:32:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -156,10 +156,10 @@ if os.path.exists("data.csv"):
     df = pd.read_csv("data.csv")
 user_data = df[df["user"] == st.session_state["user"]]
 
-if not user_data.empty:
-    st.line_chart(user_data["co2"])
-    else:
-         st.info("No data yet")
+    if not user_data.empty:
+        st.line_chart(user_data["co2"])
+        else:  
+            st.info("No data yet")
 else:
     st.info("No data file found")
 #----sql dashboard----

482683ed5db35aa3be927eb1824f7b6a43e8972f
Saurabh Kumar <saurabh7070006797@gmail.com>
2026-04-04 16:29:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -153,38 +153,39 @@ st.success("Saved!")
 st.subheader("📊 Dashboard")
 
 if os.path.exists("data.csv"):
-df = pd.read_csv("data.csv")
+    df = pd.read_csv("data.csv")
 user_data = df[df["user"] == st.session_state["user"]]
 
 if not user_data.empty:
-st.line_chart(user_data["co2"])
+    st.line_chart(user_data["co2"])
 else:
-st.info("No data yet")
+    st.info("No data yet")
 else:
-st.info("No data file found")
+    st.info("No data file found")
 #----sql dashboard----
 st.subheader("📊 Advanced SQL History")
 
 sql_data = fetch_records(st.session_state["user"])
 
 if sql_data:
-df_sql = pd.DataFrame(sql_data, columns=["CO2", "Time"])
-st.dataframe(df_sql)
+    
+    df_sql = pd.DataFrame(sql_data, columns=["CO2", "Time"])
+    st.dataframe(df_sql)
 else:
-st.info("No SQL data yet")
+    st.info("No SQL data yet")
 
 
 # ---------- SOLUTION ----------
 if menu == "Solution":
-banner("https://images.unsplash.com/photo-1470770841072-f978cf4d019e")
-st.title("🌍 Eco Solutions & Impact")
-carbon_problems()
-st.info("Without action, carbon emissions can cause:")
-st.markdown("""
-- Heatwaves & droughts 🌞
-- Flooding & storms 🌊
-- Species extinction 🐦🐘
-""")
+    banner("https://images.unsplash.com/photo-1470770841072-f978cf4d019e")
+    st.title("🌍 Eco Solutions & Impact")
+    carbon_problems()
+    st.info("Without action, carbon emissions can cause:")
+    st.markdown("""
+     - Heatwaves & droughts 🌞
+    - Flooding & storms 🌊
+    - Species extinction 🐦🐘
+    """)
 streamlit_globe(pointsData=[{"lat":20.59,"lng":78.96}],
         labelsData=[{"lat":20.59,"lng":78.96,"text":"India"}])
 st.success("🌳 Plant trees, reduce CO₂")
